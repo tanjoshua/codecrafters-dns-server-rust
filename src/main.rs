5bfb3ce4130381e4538bc7a6773d9ff072a13557
Josh <jtanjoshua@gmail.com>
2026-01-13 15:25:39 +0800

    dns message headers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
 use std::net::UdpSocket;
+mod headers;
+use headers::Headers;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,7 +15,22 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let headers = Headers {
+                    packet_id: 1234,
+                    query_response_indicator: true,
+                    operation_code: 0,
+                    authoritative_answer: false,
+                    truncation: false,
+                    recursion_desired: false,
+                    recursion_available: false,
+                    reserved: 0,
+                    response_code: 0,
+                    question_count: 0,
+                    answer_record_count: 0,
+                    authority_record_count: 0,
+                    additional_record_count: 0,
+                };
+                let response: Vec<u8> = headers.into();
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
